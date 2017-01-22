0c8fc8dbd7c1c83ac0cda8a9f25af399c395faa7
Marko Manninen <elonmedia@gmail.com>
2017-01-22 18:04:44 +0200

    bower instantiation

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+define('SERVER_ROOT', dirname($_SERVER['SCRIPT_NAME']).'/');
+
+include __DIR__.'/src/elonmedia/plcparser/php/bootstrap.php';
+
+?>
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <title>PLCParser - Prepositional logic clause parser (Python, PHP, Javascript)</title>
+        <link rel="stylesheet" href="./src/elonmedia/plcparser/css/style.css">
+    </head>
+    <body>
+
+    </body>
+</html>
