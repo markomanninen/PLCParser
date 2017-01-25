68022ac9b11d41406e66dbded1d721a4037de32f
Marko Manninen <elonmedia@gmail.com>
2017-01-25 21:38:47 +0200

    added bootstrap for dev

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $o = PLCPArser::parseInput($i);
         <meta charset="UTF-8">
         <title>PLCParser - Prepositional logic clause parser (Python, PHP, Javascript)</title>
         <link rel="stylesheet" href="./src/elonmedia/plcparser/css/style.css" />
+        <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap.min.css" />
         <script src="./bower_components/jquery/dist/jquery.min.js"></script>
         <script src="./dist/PLCParser.min.js"></script>
         <!-- jquery -->
@@ -63,16 +64,31 @@ $o = PLCPArser::parseInput($i);
 			        return result
 			    }
 
-			function evaluate(s) {
-				return PLCParser.evaluateInput(s)
+			function evaluate(s, table) {
+				return PLCParser.evaluateInput(s, table)
 			}
 
-			function deformat(s) {
-				return PLCParser.deformat(PLCParser.parseInput(s))
+			function deformat(s, short, first) {
+				return PLCParser.deformat(PLCParser.parseInput(s), short, first)
 			}
 
 			function log(s) {
 				$('#result').html(s);
+			}
+
+			function change_deformat() {
+				var v = $('#input').val()
+				var o = validate(v)
+				if (!o) {
+					log(v + " = " + 'undefined')
+				} else {
+					var s = deformat(v, 
+							$('#short').prop("checked"), 
+							$('#firstonly').prop("checked"))
+					var t = JSON.parse($('#table').text())
+					var e = evaluate(v, t)
+					log(s + " = " + e || 'false')
+				}
 			}
 
 			function keyup() {
@@ -83,8 +99,11 @@ $o = PLCPArser::parseInput($i);
 					color = 'pink'
 					log(v + " = " + 'undefined')
 				} else {
-					var s = deformat(v)
-					var e = evaluate(v)
+					var s = deformat(v, 
+							$('#short').prop("checked"), 
+							$('#firstonly').prop("checked"))
+					var t = JSON.parse($('#table').text())
+					var e = evaluate(v, t)
 					log(s + " = " + e || 'false')
 				}
 				$(this).css('background-color', color)
@@ -93,18 +112,38 @@ $o = PLCPArser::parseInput($i);
 			// Shorthand for $( document ).ready()
 			$(function() {
 			    $( "#input" ).on('keyup', keyup);
+			    $( "#short" ).on('change', change_deformat);
+			    $( "#firstonly" ).on('change', change_deformat);
 			    $( "#input" ).trigger('keyup');
 			});
 
         </script>
     </head>
     <body>
-    	<h3>Prepositional logic clause</h3>
-    	<h4>Javascript</h4>
-		<p><textarea id="input" cols="40" rows="4">(1 1 (1 0))</textarea></p>
+    	<div class="container">
+
+		<a name="top"></a>
+
+		<div class="page-header">
+			<h1>Prepositional logic clause</h1>
+		</div>
+
+    	<h2>Javascript</h2>
+    	
+		<p><input type="text" id="input" value="(1 1 (1 0))" /></p>
+		<p><textarea id="table" cols="40" rows="4">
+{
+	"T": 1, "T": "1",
+	"F": false, "f": "false"
+}</textarea></p>
+		<p>
+			Short: <input type="checkbox" id="short" value="1" />
+			First only: <input type="checkbox" id="firstonly" value="1" />
+		</p>
 		<pre id="result"></pre>
 		<h4>PHP</h4>
     	<p><?=$i?> -> </p>
     	<pre><?=print_r($o, 1)?></pre>
+    	</div>
     </body>
 </html>
