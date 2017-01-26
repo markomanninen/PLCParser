90a995e3e5ad60979b5da60c04ad825bcff9099a
Marko Manninen <elonmedia@gmail.com>
2017-01-26 21:56:49 +0200

    bootstrapped index

--- index.php
+++ index.php
@@ -10,69 +10,37 @@ $o = PLCPArser::parseInput($i);
 ?>
 <!DOCTYPE html>
 <html>
-    <head lang="en">
-        <meta charset="UTF-8">
-        <title>PLCParser - Prepositional logic clause parser (Python, PHP, Javascript)</title>
-        <link rel="stylesheet" href="./src/elonmedia/plcparser/css/style.css" />
-        <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap.min.css" />
-        <script src="./bower_components/jquery/dist/jquery.min.js"></script>
-        <script src="./dist/PLCParser.min.js"></script>
-        <!-- jquery -->
-        <script>
-        	
-        	Array.prototype.last = function () {
+	<head lang="en">
+		<meta charset="UTF-8">
+		<title>PLCParser - Prepositional logic clause parser (Python, PHP, Javascript)</title>
+		<link rel="stylesheet" href="./src/elonmedia/plcparser/css/style.css" />
+		<link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap.min.css" />
+		<script src="./bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="./bower_components/jquery-ui/jquery-ui.min.js"></script>
+		<script src="./bower_components/bootstrap/dist/css/bootstrap.min.js"></script>
+		<script src="./bower_components/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML&delayStartupUntil=configured"></script>
+		<script src="./dist/PLCParser.min.js"></script>
+		<!-- jquery -->
+		<script>
+			
+			Array.prototype.last = function () {
 			  return this[this.length - 1];
 			};
 
 			function validate(s) {
-			        var result = true
-			        var stack = []
-			        var current, previous, prev, i, len=s.length;
-			        for (i = 0; i < len; i++) {
-			            current = s.substring(i, i+1)
-			            if (prev == '\\') {
-			            	prev = current
-			            	continue;
-			             }
-			            previous = stack.last()
-			            if (
-			            ((current == '(' || current == '[' || current == '{') && previous != "'" && previous != "'" ) ||
-			                ((current == "'" && previous != "'" && previous != '\\')) ||
-			                 (current == '"' && previous != '"' && previous != '\\')) {
-			                stack.push(current);
-			            } else if(
-			            ((current == ')' || current == ']' || current == '}' ) && previous != "'" && previous != '"') || 
-			             								  current == "'" ||
-			             								  current == '"') {
-			                if (stack.length == 0) {
-			                    result = false
-			                } else {
-			                    if((current == ')' && previous == '(') ||
-			                       (current == ']' && previous == '[') ||
-			                       (current == '}' && previous == '{') ||
-			                       (current == '"' && previous == '"') ||
-			                       (current == "'" && previous == "'")) {
-			                        stack.pop()
-			                    } else {
-			                        result = false
-			                    }
-			                }
-			            }
-			            prev = current
-			        }
-			        if (stack.length>0) result = false;
-			        return result
-			    }
+				return PLCParser.validateInput(s)
+			}
 
 			function evaluate(s, table) {
 				return PLCParser.evaluateInput(s, table)
 			}
 
-			function deformat(s, short, first) {
-				return PLCParser.deformat(PLCParser.parseInput(s), short, first)
+			function deformat(s, short, first, latex) {
+				return PLCParser.deformatInput(PLCParser.parseInput(s), short, first, latex)
 			}
 
 			function log(s) {
+				$('#result').trigger('change')
 				$('#result').html(s);
 			}
 
@@ -83,67 +51,155 @@ $o = PLCPArser::parseInput($i);
 					log(v + " = " + 'undefined')
 				} else {
 					var s = deformat(v, 
-							$('#short').prop("checked"), 
-							$('#firstonly').prop("checked"))
+							$('.radio input:checked').val() == "2", 
+							$('#firstonly').prop("checked"),
+							$('.radio input:checked').val() == "3")
 					var t = JSON.parse($('#table').text())
 					var e = evaluate(v, t)
 					log(s + " = " + e || 'false')
 				}
 			}
 
+			var last_input = '', prev2 = '', color = ''
+			var _color = 'lightgreen'
 			function keyup() {
-				var color = "white"
-				var v = $(this).val()
-				var o = validate(v)
-				if (!o) {
-					color = 'pink'
-					log(v + " = " + 'undefined')
-				} else {
-					var s = deformat(v, 
-							$('#short').prop("checked"), 
-							$('#firstonly').prop("checked"))
-					var t = JSON.parse($('#table').text())
-					var e = evaluate(v, t)
-					log(s + " = " + e || 'false')
+				//  input value
+				var v = $(this).val().trim()
+				// if last trimmed input value was different, then continue
+				if (v != last_input) {
+					// update last input
+					last_input = v
+					color = "white"
+					_color = 'lightgreen'
+					var r, o = validate(v)
+					if (!o) {
+						color = 'pink'
+						_color = 'pink'
+						$('#object').text(JSON.stringify({}))
+						r = v + " = " + 'undefined'
+						log(r)
+						prev2 = r
+					} else {
+						var s = deformat(v,
+								$('.radio input:checked').val() == "2", 
+								$('#firstonly').prop("checked"),
+								$('.radio input:checked').val() == "3")
+						var t = JSON.parse($('#table').text())
+						var e = evaluate(v, t)
+						r = s + " = " + e || 'false'
+						if (prev2 != r) {
+							$('#object').text(JSON.stringify(PLCParser.parseInput(s)))
+							log(r)
+							prev2 = r
+						}
+					}
+					$(this).css('background-color', color)
 				}
-				$(this).css('background-color', color)
 			}
 
 			// Shorthand for $( document ).ready()
 			$(function() {
-			    $( "#input" ).on('keyup', keyup);
-			    $( "#short" ).on('change', change_deformat);
-			    $( "#firstonly" ).on('change', change_deformat);
-			    $( "#input" ).trigger('keyup');
+				$( "#input" ).on('keyup', keyup);
+				$( ".radio input" ).on('change', change_deformat);
+				$( "#firstonly" ).on('change', change_deformat);
+				$( "#result" ).change(function () {
+					  $(this).effect("highlight", {color:_color}, 1000);
+				});
+				$( "#input" ).trigger('keyup');
 			});
 
-        </script>
-    </head>
-    <body>
-    	<div class="container">
-
-		<a name="top"></a>
+		</script>
+		<style>
+		body {
+			font-size: 160%;
+		}
+		</style>
+	</head>
+	<body>
+		<div class="container">
 
 		<div class="page-header">
 			<h1>Prepositional logic clause</h1>
 		</div>
 
-    	<h2>Javascript</h2>
-    	
-		<p><input type="text" id="input" value="(1 1 (1 0))" /></p>
-		<p><textarea id="table" cols="40" rows="4">
-{
+
+		<div class="form-group">
+			<p>Symbols that can be used for propositional logic clauses:</p>
+			<ul class="list-group">
+			<li class="list-group-item">and , &amp; , ∧</li>
+			<li class="list-group-item">or , | , ∨</li>
+			<li class="list-group-item">not , ! , ¬</li>
+			<li class="list-group-item">xor , ^ , ⊕</li>
+			<li class="list-group-item">1 , true</li>
+			<li class="list-group-item">0 , false</li>
+			</ul>
+		</div>
+
+		<div class="panel-group">
+				<div class="panel panel-primary">
+				  <div class="panel-heading">Javascript</div>
+				  <div class="panel-body">
+				  </div>
+				  <div class="panel-body">
+					<div class="form-group">
+						<label for="input">Input:</label>
+						<input type="text" id="input" value="(1 1 (1 0))" class="form-control" />
+					</div>
+
+					<div class="form-group">
+						<label for="table">Configuration:</label>
+						<textarea id="table" class="form-control" rows="5">{
 	"T": 1, "T": "1",
 	"F": false, "f": "false"
-}</textarea></p>
-		<p>
-			Short: <input type="checkbox" id="short" value="1" />
-			First only: <input type="checkbox" id="firstonly" value="1" />
-		</p>
-		<pre id="result"></pre>
-		<h4>PHP</h4>
-    	<p><?=$i?> -> </p>
-    	<pre><?=print_r($o, 1)?></pre>
-    	</div>
-    </body>
+}</textarea>
+					</div>
+					<div class="form-group">
+						<div class="radio">
+							<label><input checked="checked" name="type" type="radio" id="type1" value="1">Word</label>
+						</div>
+						<div class="radio">
+							<label><input name="type" type="radio" id="type2" value="2">Short</label>
+						</div>
+						<div class="radio">
+							<label><input name="type" type="radio" id="type3" value="3">Math</label>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="checkbox">
+							<label><input type="checkbox" id="firstonly" value="1">First only</label>
+						</div>
+					</div>
+					<div>
+						<label>Formatted and evaluated expression:</label>
+					</div>
+					<pre id="result"></pre>
+					<div>
+						<label>JSON object:</label>
+					</div>
+					<pre id="object"></pre>
+					<div>
+						<label>JSON validation schmema:</label>
+					</div>
+					<pre id="schema"></pre>
+					<div>$$ \left( \left( 1 \land \left( 0 \lor \lnot 0 \right)  \land \oplus \left( 1 \lor 0 \right)  \right)  \right) = True $$</div>
+				  </div>
+			</div>
+				<div class="panel panel-primary">
+				  <div class="panel-heading">PHP</div>
+				  <div class="panel-body">
+					<p><?=$i?> -> </p>
+					<pre><?=print_r($o, 1)?></pre>
+				  </div>
+			</div>
+				<div class="panel panel-primary">
+				  <div class="panel-heading">Python</div>
+				  <div class="panel-body">...</div>
+			</div>
+	  </div>
+
+		</div><!-- container -->
+		<script type="text/javascript">
+		  MathJax.Hub.Configured()
+		</script>
+	</body>
 </html>
