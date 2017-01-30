9bdeab2e8cdb6978a9c92190ae534187bea02f23
Marko Manninen <elonmedia@gmail.com>
2017-01-30 17:36:21 +0200

    evaluate method tests

--- test/python/test.py
+++ test/python/test.py
@@ -8,6 +8,7 @@ class PLCParserTestCase(unittest.TestCase):
 
 	def runTest(self):
 
+		# PARSE
 		self.assertEqual(parseInput(""), (True, []), 'empty input')
 		self.assertEqual(parseInput("and or ! anything"), (True, []), 'without any parentheses')
 
@@ -76,6 +77,82 @@ class PLCParserTestCase(unittest.TestCase):
 			self.assertEqual(c.parse('[´A´]'), (True, [['A']]), 'right parentheses and wrappers')
 		
 		self.assertEqual(c.parse("['A']"), (True, [["'A'"]]), 'wrong wrappers')
+		
+		# EVALUATE
+		# basic
+		assert evaluateInput('(1 1)') == True
+		assert evaluateInput('(1 0)') == False
+		assert evaluateInput('(1 or 1)') == True
+		assert evaluateInput('(1 or 0)') == True
+		# true false literals
+		assert evaluateInput('(true true)') == True
+		assert evaluateInput('(true false)') == False
+		assert evaluateInput('(true or true)') == True
+		assert evaluateInput('(true or false)') == True
+		# group
+		assert evaluateInput('(1 (0 0))') == False
+		assert evaluateInput('(1 (1 0))') == True
+		assert evaluateInput('(1 (1 1))') == True
+		assert evaluateInput('(0 (1 1))') == False
+		# multiple groups
+		assert evaluateInput('(1 (1 0) (1 0))') == True
+		assert evaluateInput('(1 and (1 or 0) and (1 or 0))') == True
+		assert evaluateInput('(1 or (1 and 0) or (1 and 0))') == True
+		assert evaluateInput('(0 or (1 and 0) or (1 and 0))') == False
+		# NOT
+		assert evaluateInput('!(1 (0 0))') == True
+		assert evaluateInput('(!0 1)') == True
+		assert evaluateInput('(1 !(0 0))') == True
+		# and or chars
+		assert evaluateInput('(0 | 1)') == True
+		assert evaluateInput('(0 & 1)') == False
+		# and or math symbols
+		assert evaluateInput('(0 ∧ 1)') == True
+		assert evaluateInput('(0 ∧ 1)') == False
+		# XOR single
+		assert evaluateInput('(^(1))') == False
+		assert evaluateInput('(^(0))') == False
+		# NOT XOR
+		assert evaluateInput('(!^(1))') == True
+		assert evaluateInput('(!^(0))') == True
+		# XOR double
+		assert evaluateInput('(^(1 0))') == True
+		assert evaluateInput('(^(0 0))') == False
+		assert evaluateInput('(^(1 1))') == False
+		assert evaluateInput('(^(0 1))') == True
+		# XOR multiple
+		assert evaluateInput('(^(1 0 0))') == True
+		assert evaluateInput('(^(0 0 0))') == False
+		assert evaluateInput('(^(1 1 0))') == False
+		assert evaluateInput('(^(0 1 0))') == True
+		assert evaluateInput('(^(1 1 1))') == False
+		# XAND single
+		assert evaluateInput('(+(1))') == False
+		assert evaluateInput('(+(0))') == False
+		# NOT XAND
+		assert evaluateInput('(!+(1))') == True
+		assert evaluateInput('(!+(0))') == True
+		# XAND double
+		assert evaluateInput('(+(1 0))') == True
+		assert evaluateInput('(+(0 0))') == False
+		assert evaluateInput('(+(1 1))') == False
+		assert evaluateInput('(+(0 1))') == True
+		# XAND multiple
+		assert evaluateInput('(+(1 0 0))') == True
+		assert evaluateInput('(+(0 0 0))') == False
+		assert evaluateInput('(+(1 1 0))') == True
+		assert evaluateInput('(+(0 1 0))') == True
+		assert evaluateInput('(+(1 1 1))') == False
+		# deeply nested
+		assert evaluateInput('(1(1(1(1(1)))))') == True
+		# truth table
+		table = {'A': 1, 'B': 0}
+		assert evaluateInput('(A or B)', table) == True
+		assert evaluateInput('(1 ("A" "B"))', table) == True
+		
+		# DEFORMAT
+		
+		# VALIDATE
 
 if __name__ == '__main__':
     unittest.main()
