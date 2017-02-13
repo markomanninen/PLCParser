33afbe450c6a67040cd23d70e0e7bafaf2bcc011
Marko Manninen <elonmedia@gmail.com>
2017-02-13 17:12:34 +0200

    operators added to module export

--- pyPLCParser/__init__.py
+++ pyPLCParser/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from . PLCParser import PLCParser, parseInput, evaluateInput, \
-						deformatInput, validateInput, jsonSchema, \
-						ParseException
+						deformatInput, validateInput, jsonSchema, ParseException, \
+						AND_OPERATOR, XOR_OPERATOR, OR_OPERATOR, \
+						NAND_OPERATOR, XNOR_OPERATOR, NOR_OPERATOR,\
+						NOT_OPERATOR
